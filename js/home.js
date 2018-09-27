2e0e101a0c530c834a5f3b558e8e8a051c412694
vama89 <varrogancia@gmail.com>
2018-09-27 02:42:33 -0700

    cleand init and rotate cube with lights
    ditto

--- js/home.js
+++ js/home.js
@@ -1,25 +1,43 @@
 function init() {
 	var scene = new THREE.Scene();
+	var gui = new dat.GUI();
 
+	//generate objects
 	var box = getBox(1,1,1);
-
-	scene.add(box);
-
+	box.name = 'box-1';
+	var pointLight = getPointLight(1);
+	var sphere = getSphere(0.05);
 	var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight,1,1000);
+	var renderer = new THREE.WebGLRenderer();
+	var controls = new THREE.OrbitControls(camera, renderer.domElement)
+
+	//add objects to the scene
+	scene.add(box);
+	pointLight.add(sphere);
+	scene.add(pointLight);
+
+	//positions of objects
 	camera.position.x=1;
 	camera.position.y=2;
 	camera.position.z=5;
 	camera.lookAt(new THREE.Vector3(0,0,0));
+
+	pointLight.position.y = 1.25;
 	
-	var renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setClearColor('rgb(120,120,120)');
+
+	//rendering onto the screen
 	document.getElementById('webgl').appendChild(renderer.domElement);
-	renderer.render(scene, camera);
+	
+	update(renderer,scene, camera, controls);
+
+	return scene;
 };
 
 function getBox(w,h,d){
 	var geometry = new THREE.BoxGeometry(w,h,d);
-	var material = new THREE.MeshBasicMaterial({
+	var material = new THREE.MeshPhongMaterial({
 		color: 0x00ff00
 	});
 	var mesh = new THREE.Mesh(
@@ -29,4 +47,39 @@ function getBox(w,h,d){
 	return mesh;
 }
 
-init();
+function getSphere(size){
+	var geometry = new THREE.SphereGeometry(size, 24, 24);
+	var material = new THREE.MeshBasicMaterial({
+		color: 'rgb(255, 255, 255)'
+	});
+	var mesh = new THREE.Mesh(
+		geometry,
+		material
+		);
+	return mesh;
+}
+
+function getPointLight(intensity){
+	var light = new THREE.PointLight(0xffffff, intensity);
+
+	return light;
+}
+
+function update(renderer, scene, camera, controls){
+	renderer.render(
+		scene,
+		camera
+	);
+
+	var box = scene.getObjectByName('box-1');
+	box.rotation.y += 0.001;
+	box.rotation.z += 0.001;
+
+	controls.update();
+
+	requestAnimationFrame(function(){
+		update(renderer,scene, camera, controls);
+	})
+}
+
+var scene = init();
