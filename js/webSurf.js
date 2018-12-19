04673e31c36458993f41695edb96a1465d61663f
vama89 <varrogancia@gmail.com>
2018-12-19 14:28:55 -0800

    moved key presses outside init
    controls now work but it is yanked above. it moves though.

--- js/webSurf.js
+++ js/webSurf.js
@@ -7,8 +7,51 @@ function init() {
 	document.body.appendChild( container );
 
 	//generate objects
+
 	var box = getBox(1,1,1);
 	box.name = 'box-1';
+	var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+	scene.add( light );
+	var pointLight = getPointLight(1);
+	var sphere = getSphere(0.05);
+	var plane = getPlane(5,5)
+
+	//Controllers PointLock
+	var raycaster;
+	raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
+
+	var blocker = document.getElementById( 'blocker' );
+	var instructions = document.getElementById( 'instructions' );
+	//Controllers PointLock End
+
+	var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight,1,1000);
+	var renderer = new THREE.WebGLRenderer();
+	//var controls = new THREE.OrbitControls(camera, renderer.domElement)
+	var controls = new THREE.PointerLockControls( camera );
+//CONTROLS
+				instructions.addEventListener( 'click', function () {
+
+					controls.lock();
+
+				}, false );
+
+				controls.addEventListener( 'lock', function () {
+
+					instructions.style.display = 'none';
+					blocker.style.display = 'none';
+
+				} );
+
+				controls.addEventListener( 'unlock', function () {
+
+					blocker.style.display = 'block';
+					instructions.style.display = '';
+
+				} );
+
+				scene.add( controls.getObject() );
+//CONTROLS END
+
 /*
 	for(i=0; i<1000; i++){
 
@@ -31,19 +74,6 @@ function init() {
 	scene.add(distanceBox);
 	}
 */
-	var light = new THREE.AmbientLight( 0x404040 ); // soft white light
-	scene.add( light );
-
-	var objects=[];
-
-	var pointLight = getPointLight(1);
-	var sphere = getSphere(0.05);
-	var plane = getPlane(5,5)
-
-	var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight,1,1000);
-	var renderer = new THREE.WebGLRenderer();
-	var controls = new THREE.OrbitControls(camera, renderer.domElement)
-
 	//add objects to the scene
 	scene.add(box);
 	pointLight.add(sphere);
@@ -53,20 +83,15 @@ function init() {
 	objects.push(plane);
 
 	//positions of objects
-	
 	camera.position.x=1;
 	camera.position.y=2;
 	camera.position.z=5;
-	camera.lookAt(new THREE.Vector3(0,0,0));
-	
-
+	//camera.lookAt(new THREE.Vector3(0,0,0));
 	pointLight.position.y = 1.25;
-
-	//radios
 	plane.rotation.x = Math.PI/2;
-
 	box.position.y = box.geometry.parameters.height/2;
-	document.addEventListener('mousedown', playSound, false);
+
+	//document.addEventListener('mousedown', playSound, false);
 
 //iono how to pull this out of the init function
 //This will be the place for the controls
@@ -104,10 +129,11 @@ console.log(objects.length)
 
 	//rendering onto the screen
 	document.getElementById('webgl').appendChild(renderer.domElement);
+
 	stats = new Stats();
 	container.appendChild( stats.dom )
 
-	update(renderer,scene, camera, controls, stats);
+	update(renderer,scene, camera, controls, stats, raycaster);
 
 	return scene;
 };
@@ -174,24 +200,146 @@ function getPlane(w,h){
 }
 
 
-function update(renderer, scene, camera, controls, stats){
+function update(renderer,scene, camera, controls, stats, raycaster){
 	renderer.render(
 		scene,
 		camera
 	);
+	//var box = scene.getObjectByName('box-1');
 
-	var box = scene.getObjectByName('box-1');
-
-	box.rotation.y += 0.001;
-	box.rotation.z += 0.001;
+	//box.rotation.y += 0.001;
+	//box.rotation.z += 0.001;
 
 	//controls.update();
+//CONTROLS
+	if ( controls.isLocked === true ) {
+		raycaster.ray.origin.copy( controls.getObject().position );
+		raycaster.ray.origin.y -= 10;
 
+		var intersections = raycaster.intersectObjects( objects );
+
+		var onObject = intersections.length > 0;
+
+		var time = performance.now();
+		var delta = ( time - prevTime ) / 1000;
+
+		velocity.x -= velocity.x * 10.0 * delta;
+		velocity.z -= velocity.z * 10.0 * delta;
+
+		velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+		direction.z = Number( moveForward ) - Number( moveBackward );
+		direction.x = Number( moveLeft ) - Number( moveRight );
+		direction.normalize(); // this ensures consistent movements in all directions
+
+		if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
+		if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
+
+		if ( onObject === true ) {
+
+			velocity.y = Math.max( 0, velocity.y );
+			canJump = true;
+
+		}
+
+		controls.getObject().translateX( velocity.x * delta );
+		controls.getObject().translateY( velocity.y * delta );
+		controls.getObject().translateZ( velocity.z * delta );
+
+		if ( controls.getObject().position.y < 10 ) {
+
+			velocity.y = 0;
+			controls.getObject().position.y = 10;
+
+			canJump = true;
+
+		}
+
+		prevTime = time;
+
+	}
+//CONTROLS END
 	requestAnimationFrame(function(){
-		update(renderer,scene, camera, controls, stats);
+		update(renderer,scene, camera, controls, stats, raycaster);
 	})
 
 	stats.update();
 }
+
+var objects=[];
+var moveForward = false;
+var moveBackward = false;
+var moveLeft = false;
+var moveRight = false;
+var canJump = false;
+
+var prevTime = performance.now();
+var velocity = new THREE.Vector3();
+var direction = new THREE.Vector3();
+var vertex = new THREE.Vector3();
+var color = new THREE.Color();
+
+var onKeyDown = function ( event ) {
+
+	switch ( event.keyCode ) {
+
+		case 38: // up
+		case 87: // w
+			moveForward = true;
+			break;
+
+		case 37: // left
+		case 65: // a
+			moveLeft = true;
+			break;
+
+		case 40: // down
+		case 83: // s
+			moveBackward = true;
+			break;
+
+		case 39: // right
+		case 68: // d
+			moveRight = true;
+			break;
+
+		case 32: // space
+			if ( canJump === true ) velocity.y += 350;
+			canJump = false;
+			break;
+
+	}
+};
+
+var onKeyUp = function ( event ) {
+
+	switch ( event.keyCode ) {
+
+		case 38: // up
+		case 87: // w
+			moveForward = false;
+			break;
+
+		case 37: // left
+		case 65: // a
+			moveLeft = false;
+			break;
+
+		case 40: // down
+		case 83: // s
+			moveBackward = false;
+			break;
+
+		case 39: // right
+		case 68: // d
+			moveRight = false;
+			break;
+
+	}
+
+};
+
+document.addEventListener( 'keydown', onKeyDown, false );
+document.addEventListener( 'keyup', onKeyUp, false );
 
 var scene = init();
