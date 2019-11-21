1ed120738a7c9a604e97b4a21cf536285b3b8df8
vama89 <varrogancia@gmail.com>
2019-11-21 09:17:39 -0800

    rearranged js imports to proper starter example and used navbar for top with button link ready for game download

--- main.py
+++ main.py
@@ -31,15 +31,12 @@ class Handler(webapp2.RequestHandler):
   def write(self, *a, **kw):
     self.response.out.write(*a, **kw)
 
-
   def render_str(self, template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
-
   def render(self, template, **kw):
     self.write(self.render_str(template, **kw))
-
 
 class MainHandler(Handler):
     def get(self):
